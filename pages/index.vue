80da7afd3c3ca79afeda9d5cebf58971774226a5
Muhammad Iqbal A.R <miqbalar12@gmail.com>
2025-06-07 17:22:08 +0700

    init

---
+++ pages/index.vue
@@ -0,0 +1,6 @@
+<template>
+  <div class="container mt-5">
+    <h1 class="text-primary">Halo dari Nuxt + Bootstrap!</h1>
+    <button class="btn btn-success">Tombol Bootstrap</button>
+  </div>
+</template>
