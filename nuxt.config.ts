2dd59961105277ec540b299036bceec9f7e75081
Muhammad Iqbal A.R <miqbalar12@gmail.com>
2025-06-07 17:36:14 +0700

    chore(nuxt): setting sass + css

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,14 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: "2025-05-15",
   devtools: { enabled: false },
-  css: ["bootstrap/dist/css/bootstrap.min.css"],
+  css: [
+    "bootstrap/dist/css/bootstrap.min.css",
+    // css files
+    "@/assets/template/css/style.css",
+    "@/assets/template/css/mediaqueries.css",
+    "@/assets/template/css/owl.carousel.min.css",
+    "@/assets/template/css/owl.theme.default.min.css",
+    // scss files
+    "@/assets/scss/style.scss",
+  ],
 });
