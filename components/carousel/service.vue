b242a89e18743b26c6626269bfb3cf99f687ad64
Muhammad Iqbal A.R <miqbalar12@gmail.com>
2025-06-08 10:59:26 +0700

    fix(service): fixing services

---
+++ components/carousel/service.vue
@@ -0,0 +1,139 @@
+<script setup>
+import "vue3-carousel/carousel.css";
+import { Carousel, Slide, Pagination, Navigation } from "vue3-carousel";
+
+import petGrommingImages from "~/assets/template/images/services_image1.png";
+import petCareImages from "~/assets/template/images/services_image2.png";
+import veterinaryImages from "~/assets/template/images/services_image3.png";
+import petTrainingImages from "~/assets/template/images/services_image4.png";
+
+const carouselConfig = {
+  itemsToShow: 4,
+  wrapAround: true,
+  snapAlign: "left",
+  autoplay: "3000",
+};
+const services = [
+  {
+    img: petGrommingImages,
+    title: "Pet Grooming",
+    desc: "Quam nihil molestiae vel illum rui dolorem em fugiat voluptas nulla...",
+    boxClass: "box1",
+  },
+  {
+    img: petCareImages,
+    title: "Pet Care",
+    desc: "Ruam nihil molestiae vel illum rui dolorem em fugiat voluptas nulla...",
+    boxClass: "box2",
+  },
+  {
+    img: veterinaryImages,
+    title: "Vaterinary",
+    desc: "Guam nihil molestiae vel illum rui dolorem em fugiat voluptas nulla...",
+    boxClass: "box3",
+  },
+  {
+    img: petTrainingImages,
+    title: "Pet Training",
+    desc: "Euam nihil molestiae vel illum rui dolorem em fugiat voluptas nulla...",
+    boxClass: "box4",
+  },
+];
+const currentSlide = ref(0);
+const carouselRef = ref(null);
+
+function onSlideChange(index) {
+  currentSlide.value = index;
+}
+function goToSlide(index) {
+  if (carouselRef.value) {
+    currentSlide.value = index;
+    carouselRef.value.slideTo(index);
+  }
+}
+</script>
+
+<template>
+  <Carousel
+    ref="carouselRef"
+    v-bind="carouselConfig"
+    @slide-start="onSlideChange"
+  >
+    <Slide v-for="(service, idx) in services" :key="idx">
+      <div class="item">
+        <div :class="['services_box', service.boxClass]">
+          <figure>
+            <img :src="service.img" alt="" class="img-fluid hover-effect" />
+          </figure>
+          <h3>{{ service.title }}</h3>
+          <p class="text-size-18">
+            {{ service.desc }}
+          </p>
+          <div class="btn_wrapper">
+            <a class="text-decoration-none" href="services.html">
+              Read more<i class="fa-solid fa-arrow-right"></i>
+            </a>
+          </div>
+        </div>
+      </div>
+    </Slide>
+
+    <template #addons>
+      <div class="custom-pagination">
+        <button
+          v-for="(item, index) in services"
+          :key="index"
+          @click="goToSlide(index)"
+          :class="{ active: currentSlide.currentSlideIndex === index }"
+        ></button>
+      </div>
+    </template>
+  </Carousel>
+</template>
+
+<style scoped>
+.carousel {
+  position: relative;
+}
+
+:deep(.carousel__slide) {
+  padding: 5px;
+}
+
+:deep(.carousel__prev),
+:deep(.carousel__next) {
+  background-color: rgba(0, 0, 0, 0.3);
+  border-radius: 50%;
+  width: 48px;
+  height: 48px;
+}
+
+:deep(.carousel__pagination) {
+  bottom: -10px !important;
+}
+</style>
+<style scoped lang="scss">
+.custom-pagination {
+  display: flex;
+  justify-content: center;
+  gap: 12px;
+  margin-top: 0.2rem;
+
+  button {
+    width: 12px;
+    height: 12px;
+    border-radius: 50%;
+    background-color: #e5e7eb;
+    border: none;
+    cursor: pointer;
+    transition: background-color 0.3s ease;
+
+    &.active {
+      background-color: #f59e0b;
+      width: 24px;
+      height: 12px;
+      border-radius: 6px;
+    }
+  }
+}
+</style>
