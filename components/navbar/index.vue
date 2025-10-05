b97bb19657e40c10faa957881dee15bf11a3d337
Muhammad Iqbal A.R <miqbalar12@gmail.com>
2025-10-05 16:01:23 +0700

    fix(navbar): link active bar

--- components/navbar/index.vue
+++ components/navbar/index.vue
@@ -1,16 +1,59 @@
+<script lang="ts" setup>
+import { computed } from "vue";
+
+const route = useRoute();
+
+const isActive = computed(() => (path: string) => {
+  return route.path === path;
+});
+const menuItems = computed(() => [
+  {
+    name: "Home",
+    path: "/",
+    isActive: route.path === "/",
+  },
+  {
+    name: "About us",
+    path: "/about",
+    isActive: route.path === "/about",
+  },
+  {
+    name: "Services",
+    path: "/services",
+    isActive: route.path === "/services",
+  },
+  {
+    name: "Packages",
+    path: "/packages",
+    isActive: route.path === "/packages",
+  },
+  {
+    name: "Gallery",
+    path: "/gallery",
+    isActive: route.path === "/gallery",
+  },
+  {
+    name: "Contact Us",
+    path: "/contact",
+    isActive: route.path === "/contact",
+    isButton: true,
+  },
+]);
+</script>
 <template>
   <header>
     <div class="main_header">
       <div class="container-fluid">
         <nav class="navbar navbar-expand-lg navbar-light p-0">
-          <a class="navbar-brand" href="index.html"
-            ><figure class="mb-0">
+          <NuxtLink to="/" class="navbar-brand">
+            <figure class="mb-0">
               <img
                 src="assets/images/logo/sasa_logo_preview.png"
                 alt=""
                 class="img-fluid"
-              /></figure
-          ></a>
+              />
+            </figure>
+          </NuxtLink>
           <button
             class="navbar-toggler collapsed"
             type="button"
@@ -26,25 +69,23 @@
           </button>
           <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav">
-              <li class="nav-item active">
-                <a class="nav-link" href="/">Home</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="/about">About us</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="/services">Services</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="/packages">Packages</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="/gallery">Gallery</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link default-btn contact_us" href="/contact"
-                  >Contact Us</a
+              <li
+                v-for="item in menuItems"
+                :key="item.path"
+                :class="['nav-item', { active: item.isActive }]"
+              >
+                <NuxtLink
+                  :to="item.path"
+                  :class="[
+                    'nav-link',
+                    {
+                      'default-btn contact_us': item.isButton,
+                      active: item.isActive,
+                    },
+                  ]"
                 >
+                  {{ item.name }}
+                </NuxtLink>
               </li>
             </ul>
             <div class="outer_div">
