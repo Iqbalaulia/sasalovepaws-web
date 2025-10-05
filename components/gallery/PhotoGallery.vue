b711e00157bbcc1faa9868c9b31460976677ca5f
Muhammad Iqbal A.R <miqbalar12@gmail.com>
2025-10-05 15:49:23 +0700

    feat(gallery): pages gallery

---
+++ components/gallery/PhotoGallery.vue
@@ -0,0 +1,447 @@
+<script lang="ts" setup></script>
+<template>
+  <section class="gallery_section position-relative">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-12 col-md-12 col-sm-12 col-12">
+          <div class="gallery_content">
+            <h6>Gallery</h6>
+            <h2>Our Photos Gallery</h2>
+          </div>
+        </div>
+      </div>
+      <div class="tabs-box tabs-options">
+        <ul class="nav nav-tabs">
+          <li><a class="active" data-toggle="tab" href="#all">All</a></li>
+          <li>
+            <a data-toggle="tab" href="#walking_sitting" class="walking"
+              >Walking & Sitting</a
+            >
+          </li>
+          <li>
+            <a data-toggle="tab" href="#grooming" class="grooming">Grooming</a>
+          </li>
+          <li>
+            <a data-toggle="tab" href="#training" class="training">Training</a>
+          </li>
+          <li>
+            <a data-toggle="tab" href="#health_wellness" class="health"
+              >Health & Wellness</a
+            >
+          </li>
+        </ul>
+        <div class="tab-content" data-aos="fade-up">
+          <div id="all" class="tab-pane fade in active show">
+            <div class="row position-relative">
+              <div class="col-lg-4 col-md-4 col-sm-6 col-12">
+                <div class="gallery_image">
+                  <figure class="mb-0">
+                    <img
+                      src="assets/images/gallery/gallery_image1.jpg"
+                      alt=""
+                      class="img-fluid"
+                    />
+                  </figure>
+                  <div class="overlay bottom-overlay">
+                    <div class="box_hover_effect">
+                      <a href="gallery.html" target="_blank">
+                        <i class="fa-solid fa-magnifying-glass-plus"></i>
+                      </a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div class="col-lg-4 col-md-4 col-sm-6 col-12">
+                <div class="gallery_image">
+                  <figure class="mb-0">
+                    <img
+                      src="assets/images/gallery/gallery_image2.jpg"
+                      alt=""
+                      class="img-fluid"
+                    />
+                  </figure>
+                  <div class="overlay bottom-overlay">
+                    <div class="box_hover_effect">
+                      <a href="gallery.html" target="_blank">
+                        <i class="fa-solid fa-magnifying-glass-plus"></i>
+                      </a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div class="col-lg-4 col-md-4 col-sm-6 col-12">
+                <div class="gallery_image">
+                  <figure class="mb-0">
+                    <img
+                      src="assets/images/gallery/gallery_image3.jpg"
+                      alt=""
+                      class="img-fluid"
+                    />
+                  </figure>
+                  <div class="overlay bottom-overlay">
+                    <div class="box_hover_effect">
+                      <a href="gallery.html" target="_blank">
+                        <i class="fa-solid fa-magnifying-glass-plus"></i>
+                      </a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <figure
+                class="gallery_image_right_shape mb-0 position-absolute left_right_shape"
+              >
+                <img
+                  src="assets/images/gallery/gallery_image_right_shape.png"
+                  alt=""
+                  class="img-fluid"
+                />
+              </figure>
+            </div>
+            <div class="row position-relative">
+              <div class="col-lg-4 col-md-4 col-sm-6 col-12">
+                <div class="gallery_image">
+                  <figure class="mb-0">
+                    <img
+                      src="assets/images/gallery/gallery_image4.jpg"
+                      alt=""
+                      class="img-fluid"
+                    />
+                  </figure>
+                  <div class="overlay bottom-overlay">
+                    <div class="box_hover_effect">
+                      <a href="gallery.html" target="_blank">
+                        <i class="fa-solid fa-magnifying-glass-plus"></i>
+                      </a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div class="col-lg-4 col-md-4 col-sm-6 col-12">
+                <div class="gallery_image">
+                  <figure class="mb-0">
+                    <img
+                      src="assets/images/gallery/gallery_image5.jpg"
+                      alt=""
+                      class="img-fluid"
+                    />
+                  </figure>
+                  <div class="overlay bottom-overlay">
+                    <div class="box_hover_effect">
+                      <a href="gallery.html" target="_blank">
+                        <i class="fa-solid fa-magnifying-glass-plus"></i>
+                      </a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div class="col-lg-4 col-md-4 col-sm-6 col-12">
+                <div class="gallery_image">
+                  <figure class="mb-0">
+                    <img
+                      src="assets/images/gallery/gallery_image6.jpg"
+                      alt=""
+                      class="img-fluid"
+                    />
+                  </figure>
+                  <div class="overlay bottom-overlay">
+                    <div class="box_hover_effect">
+                      <a href="gallery.html" target="_blank">
+                        <i class="fa-solid fa-magnifying-glass-plus"></i>
+                      </a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div id="walking_sitting" class="tab-pane fade">
+            <div class="row position-relative">
+              <div class="col-lg-4 col-md-4 col-sm-6 col-12">
+                <div class="gallery_image">
+                  <figure class="mb-0">
+                    <img
+                      src="assets/images/gallery/gallery_image1.jpg"
+                      alt=""
+                      class="img-fluid"
+                    />
+                  </figure>
+                  <div class="overlay bottom-overlay">
+                    <div class="box_hover_effect">
+                      <a href="gallery.html" target="_blank">
+                        <i class="fa-solid fa-magnifying-glass-plus"></i>
+                      </a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div class="col-lg-4 col-md-4 col-sm-6 col-12">
+                <div class="gallery_image">
+                  <figure class="mb-0">
+                    <img
+                      src="assets/images/gallery/gallery_image2.jpg"
+                      alt=""
+                      class="img-fluid"
+                    />
+                  </figure>
+                  <div class="overlay bottom-overlay">
+                    <div class="box_hover_effect">
+                      <a href="gallery.html" target="_blank">
+                        <i class="fa-solid fa-magnifying-glass-plus"></i>
+                      </a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div class="col-lg-4 col-md-4 col-sm-6 col-12">
+                <div class="gallery_image">
+                  <figure class="mb-0">
+                    <img
+                      src="assets/images/gallery/gallery_image3.jpg"
+                      alt=""
+                      class="img-fluid"
+                    />
+                  </figure>
+                  <div class="overlay bottom-overlay">
+                    <div class="box_hover_effect">
+                      <a href="gallery.html" target="_blank">
+                        <i class="fa-solid fa-magnifying-glass-plus"></i>
+                      </a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <figure
+                class="gallery_image_right_shape mb-0 position-absolute left_right_shape"
+              >
+                <img
+                  src="assets/images/gallery/gallery_image_right_shape.png"
+                  alt=""
+                  class="img-fluid"
+                />
+              </figure>
+            </div>
+          </div>
+          <div id="grooming" class="tab-pane fade">
+            <div class="row position-relative">
+              <div class="col-lg-4 col-md-4 col-sm-6 col-12">
+                <div class="gallery_image">
+                  <figure class="mb-0">
+                    <img
+                      src="assets/images/gallery/gallery_image4.jpg"
+                      alt=""
+                      class="img-fluid"
+                    />
+                  </figure>
+                  <div class="overlay bottom-overlay">
+                    <div class="box_hover_effect">
+                      <a href="gallery.html" target="_blank">
+                        <i class="fa-solid fa-magnifying-glass-plus"></i>
+                      </a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div class="col-lg-4 col-md-4 col-sm-6 col-12">
+                <div class="gallery_image">
+                  <figure class="mb-0">
+                    <img
+                      src="assets/images/gallery/gallery_image5.jpg"
+                      alt=""
+                      class="img-fluid"
+                    />
+                  </figure>
+                  <div class="overlay bottom-overlay">
+                    <div class="box_hover_effect">
+                      <a href="gallery.html" target="_blank">
+                        <i class="fa-solid fa-magnifying-glass-plus"></i>
+                      </a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div class="col-lg-4 col-md-4 col-sm-6 col-12">
+                <div class="gallery_image">
+                  <figure class="mb-0">
+                    <img
+                      src="assets/images/gallery/gallery_image6.jpg"
+                      alt=""
+                      class="img-fluid"
+                    />
+                  </figure>
+                  <div class="overlay bottom-overlay">
+                    <div class="box_hover_effect">
+                      <a href="gallery.html" target="_blank">
+                        <i class="fa-solid fa-magnifying-glass-plus"></i>
+                      </a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <figure
+                class="gallery_image_right_shape mb-0 position-absolute left_right_shape"
+              >
+                <img
+                  src="assets/images/gallery/gallery_image_right_shape.png"
+                  alt=""
+                  class="img-fluid"
+                />
+              </figure>
+            </div>
+          </div>
+          <div id="training" class="tab-pane fade">
+            <div class="row position-relative">
+              <div class="col-lg-4 col-md-4 col-sm-6 col-12">
+                <div class="gallery_image">
+                  <figure class="mb-0">
+                    <img
+                      src="assets/images/gallery/gallery_image1.jpg"
+                      alt=""
+                      class="img-fluid"
+                    />
+                  </figure>
+                  <div class="overlay bottom-overlay">
+                    <div class="box_hover_effect">
+                      <a href="gallery.html" target="_blank">
+                        <i class="fa-solid fa-magnifying-glass-plus"></i>
+                      </a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div class="col-lg-4 col-md-4 col-sm-6 col-12">
+                <div class="gallery_image">
+                  <figure class="mb-0">
+                    <img
+                      src="assets/images/gallery/gallery_image2.jpg"
+                      alt=""
+                      class="img-fluid"
+                    />
+                  </figure>
+                  <div class="overlay bottom-overlay">
+                    <div class="box_hover_effect">
+                      <a href="gallery.html" target="_blank">
+                        <i class="fa-solid fa-magnifying-glass-plus"></i>
+                      </a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div class="col-lg-4 col-md-4 col-sm-6 col-12">
+                <div class="gallery_image">
+                  <figure class="mb-0">
+                    <img
+                      src="assets/images/gallery/gallery_image6.jpg"
+                      alt=""
+                      class="img-fluid"
+                    />
+                  </figure>
+                  <div class="overlay bottom-overlay">
+                    <div class="box_hover_effect">
+                      <a href="gallery.html" target="_blank">
+                        <i class="fa-solid fa-magnifying-glass-plus"></i>
+                      </a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <figure
+                class="gallery_image_right_shape mb-0 position-absolute left_right_shape"
+              >
+                <img
+                  src="assets/images/gallery/gallery_image_right_shape.png"
+                  alt=""
+                  class="img-fluid"
+                />
+              </figure>
+            </div>
+          </div>
+          <div id="health_wellness" class="tab-pane fade">
+            <div class="row position-relative">
+              <div class="col-lg-4 col-md-4 col-sm-6 col-12">
+                <div class="gallery_image">
+                  <figure class="mb-0">
+                    <img
+                      src="assets/images/gallery/gallery_image4.jpg"
+                      alt=""
+                      class="img-fluid"
+                    />
+                  </figure>
+                  <div class="overlay bottom-overlay">
+                    <div class="box_hover_effect">
+                      <a href="gallery.html" target="_blank">
+                        <i class="fa-solid fa-magnifying-glass-plus"></i>
+                      </a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div class="col-lg-4 col-md-4 col-sm-6 col-12">
+                <div class="gallery_image">
+                  <figure class="mb-0">
+                    <img
+                      src="assets/images/gallery/gallery_image5.jpg"
+                      alt=""
+                      class="img-fluid"
+                    />
+                  </figure>
+                  <div class="overlay bottom-overlay">
+                    <div class="box_hover_effect">
+                      <a href="gallery.html" target="_blank">
+                        <i class="fa-solid fa-magnifying-glass-plus"></i>
+                      </a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div class="col-lg-4 col-md-4 col-sm-6 col-12">
+                <div class="gallery_image">
+                  <figure class="mb-0">
+                    <img
+                      src="assets/images/gallery/gallery_image3.jpg"
+                      alt=""
+                      class="img-fluid"
+                    />
+                  </figure>
+                  <div class="overlay bottom-overlay">
+                    <div class="box_hover_effect">
+                      <a href="gallery.html" target="_blank">
+                        <i class="fa-solid fa-magnifying-glass-plus"></i>
+                      </a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <figure
+                class="gallery_image_right_shape mb-0 position-absolute left_right_shape"
+              >
+                <img
+                  src="assets/images/gallery/gallery_image_right_shape.png"
+                  alt=""
+                  class="img-fluid"
+                />
+              </figure>
+            </div>
+          </div>
+        </div>
+      </div>
+      <figure
+        class="gallery_big_foot_shape mb-0 position-absolute top_bottom_shape"
+      >
+        <img
+          src="assets/images/gallery/gallery_big_foot_shape.png"
+          alt=""
+          class="img-fluid"
+        />
+      </figure>
+      <figure
+        class="gallery_small_foot_shape mb-0 position-absolute top_bottom_shape"
+      >
+        <img
+          src="assets/images/gallery/gallery_small_foot_shape.png"
+          alt=""
+          class="img-fluid"
+        />
+      </figure>
+    </div>
+  </section>
+</template>
---
+++ pages/gallery/index.vue
@@ -0,0 +1,31 @@
+<script lang="ts" setup>
+import Navbar from "~/components/navbar/index.vue";
+import SubBanner from "~/components/banner/sub-banner.vue";
+import Statistics from "~/components/statistic/index.vue";
+import Footer from "~/components/footer/index.vue";
+import Testimonials from "~/components/testimonials/index.vue";
+import PhotoGallery from "~/components/gallery/PhotoGallery.vue";
+</script>
+<template>
+  <section class="sub-banner-section-outer">
+    <Navbar />
+    <SubBanner
+      title="Gallery"
+      description="Renectus sit aliquam mattis orci dignissim sed saritas arcu semper felis morbi vulrautate nerue mattis assa lectus."
+      breadcrumbHome="Home"
+      breadcrumbCurrent="Gallery"
+    />
+  </section>
+  <section class="gallery_section">
+    <PhotoGallery />
+  </section>
+  <section class="testimonials_section position-relative">
+    <Testimonials />
+  </section>
+  <section class="stats_news_combo_section">
+    <Statistics />
+  </section>
+  <section class="footer-section">
+    <Footer />
+  </section>
+</template>
